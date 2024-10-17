bfa5042c239769ec300f95b421c638d21f563871
emanuelkkj <emanuelfdelima19@gmail.com>
2024-10-17 11:16:56 -0300

    MENU_COM_LINKS

---
+++ Menucomlinks/script.js
@@ -0,0 +1,15 @@
+// IDENTIFICAR ELEMENTOS HTML
+const menu = document.querySelector('.menu');
+const vermelho = document.querySelector('#vermelho');
+const verde = document.querySelector('#verde');
+const azul = document.querySelector('#azul');
+const amarelo = document.querySelector('#amarelo');
+const box = document.querySelector('.box');
+const texto = document.querySelector('#texto');
+
+
+//MANIPULAR ESSES ELEMENTOS HTML
+vermelho.addEventListener('click', function() { box.style.backgroundColor = 'red'; texto.style.color = 'white'; })
+verde.addEventListener('click', function() { box.style.backgroundColor = 'green'; texto.style.color = 'white'; })
+azul.addEventListener('click', function() { box.style.backgroundColor = 'blue'; texto.style.color = 'white'; })
+amarelo.addEventListener('click', function() { box.style.backgroundColor = 'yellow'; texto.style.color = 'white'; })
