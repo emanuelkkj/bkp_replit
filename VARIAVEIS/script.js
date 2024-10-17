2105c9071edb4c4e4e4f2521fdd7458268d62fbe
emanuelkkj <emanuelfdelima19@gmail.com>
2024-10-17 11:29:43 -0300

    VARIAVEIS

---
+++ VARIAVEIS/script.js
@@ -0,0 +1,9 @@
+// # id ou identificador
+// . class ou classe
+const comidas = document.querySelector('.comidas')
+// Prompt -> pergunta ao usuário(interage)
+const comida1 = prompt('Digite sua comida nº1: ')
+// Insere o conteúdo do usuário dentro do HTML
+comidas.innerText = comida1;
+
+//alert("Você gosta de:  " + comida1)
